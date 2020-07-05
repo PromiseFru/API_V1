e02c252aab747b8efd5266b65bcbb1081708bdbc
Promise Fru <promisefru1@gmail.com>
2020-07-05 03:02:59 +0100

    db modified

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 let sms = [
-    { _id: 1, user: "Sachin", type: "sent" },
-    { _id: 2, user: "Dhoni", type: "sent" },
-    { _id: 3, user: "Virat", type: "received" }
+    { _id: 1, user: "Sachin", type: "sms", status: "sent" },
+    { _id: 2, user: "Dhoni", type: "sms", status: "sent" },
+    { _id: 3, user: "Virat", type: "sms", status: "received" }
   ];
   module.exports = sms;
