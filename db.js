5198cf558c69cbcd36867cf635aee7ff2fb1f1c9
Promise Fru <promisefru1@gmail.com>
2020-07-05 02:55:30 +0100

    test db db.js created

---
+++ db.js
@@ -0,0 +1,6 @@
+let sms = [
+    { _id: 1, user: "Sachin", type: "sent" },
+    { _id: 2, user: "Dhoni", type: "sent" },
+    { _id: 3, user: "Virat", type: "received" }
+  ];
+  module.exports = sms;
