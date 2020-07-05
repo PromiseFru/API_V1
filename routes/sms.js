8490e719b598017b972adf12ebb52d8b281c0522
Promise Fru <promisefru1@gmail.com>
2020-07-05 03:20:40 +0100

    get all sms messages ... sms routes

--- routes/sms.js
+++ routes/sms.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const router = express.Router();
 // import db
-let sms = require("../db");
+let smsdb = require("../db");
+
+// get all SMS messages
+router.get("/:version_number/deku/user/:id/sms/all", async (req, res) => {
+    let { id } = req.params;
+    let { version_number } = req.params;
+    id = Number(id);
+    version_number = Number(version_number);
+
+    try {
+      res.status(200).json({
+        data: smsdb
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: "Some error occured",
+        err
+      });
+    }
+  });
