10d100f6e3c4a536d7bb924bd8aa36fb24f5e53d
Promise Fru <promisefru@Promises-MacBook-Air.local>
2020-07-05 02:36:33 +0100

    index.js file created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+// define port
+const port = process.env.PORT || 3001;
+// use logger
+app.use(logger('dev'));
+// use cors
+app.use(cors());
+// use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// import and use SMS route
+const smsRouter = require('./routes/SMS');
+app.use('/sms', smsRouter);
+// listen to defined port
+app.listen(port, function() {
+    console.log("Runnning on " + port);
+  });
+// export app module
+module.exports = app;
