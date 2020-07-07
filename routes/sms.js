2a22ca7e290cf2ed5fbf45539519a7c34daedae8
Promise Fru <promisefru1@gmail.com>
2020-07-07 04:54:18 +0100

    sent messages route created

--- routes/sms.js
+++ routes/sms.js
@@ -23,4 +23,24 @@ router.get("/:version_number/deku/user/:id/sms/all", async (req, res) => {
     }
   });
 
+// get user's sent SMS messages
+router.get("/:version_number/deku/user/:id/sms/:sent", async (req, res) => {
+    let { id, version_number, sent } = req.params;
+    id = Number(id);
+    version_number = Number(version_number);
+    sent = String(sent);
+
+    try {
+      let sms = smsdb.filter(sms => sms._id === id && sms.version_number === version_number && sms.status === sent);
+      res.status(200).json({
+        data: sms
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: "Some error occured",
+        err
+      });
+    }
+  });
+
   module.exports = router;
